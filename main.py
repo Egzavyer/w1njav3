e5071a7f015570c83c1188c3a9c9a0b3c43f5da7
Xavier Lermusieaux <xavier.lermusieaux@gmail.com>
2025-02-23 15:32:03 -0500

    initial reset commit

--- main.py
+++ main.py
@@ -1,30 +1,6 @@
-from PeerDiscovery import *
-from SocketInterface import *
-from ConnectionHandler import *
-from Peer import *
-from FileHandler import *
-
-
 def main():
     try:
-        fh = FileHandler("files")
-        si = SocketInterface()
-        pd = PeerDiscovery(si)
-        ch = ConnectionHandler(si, fh)
-        peer = Peer(pd, ch)
-
-        peer.startPeer()
-        while len(peer.pd.peerTable) == 0:
-            pass
-        peerChoice = "r"
-        while peerChoice == "r":
-            peerMap = peer.choosePeer()
-            peerChoice = input("")
-        print("Chosen Peer: " + peerMap[peerChoice])
-        peer.connectToPeer(peerMap[peerChoice])
-        peer.chooseFile()
-        while True:
-            pass
+        pass
     except KeyboardInterrupt:
         pass
 
